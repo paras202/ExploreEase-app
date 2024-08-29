5d8bb23fbdcddd10dc021db2193ad1651bcb6a21
Paras Singla <121365367+paras202@users.noreply.github.com>
2024-08-29 20:07:59 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" h-screen flex items-center justify-center">
-   <h1 className=" text-3xl" >Welcome</h1>
+   <h1 className=" text-3xl" >Welcome  Hello world by paras</h1>
    </div>
   );
 }
