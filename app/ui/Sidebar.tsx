ed62388a1881d44ba6b5a1dc7b12def91fcb225c
Paras Singla <121365367+paras202@users.noreply.github.com>
2024-11-24 22:31:41 +0530

    added scheduler button in sidebar

--- app/ui/Sidebar.tsx
+++ app/ui/Sidebar.tsx
@@ -94,6 +94,12 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, toggleSidebar, user })
               </button>
             </li>
             <li>
+              <button onClick={() => handleNavigation('/scheduler')} className="flex items-center w-full p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <FaUser className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                <span className="ml-3">Scheduler</span>
+              </button>
+            </li>
+            <li>
               <button onClick={handleSignOut} className="flex items-center w-full p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <FaSignOutAlt className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span className="ml-3">Sign Out</span>
